529f01503dce85c50a19fde69a8a8c123b71fe88
faaris123 <119583353+faaris123@users.noreply.github.com>
2025-01-03 00:48:24 -0600

    Create Student.jsx

---
+++ src/components/Student.jsx
@@ -0,0 +1,15 @@
+const Student = (props) => {
+    return <div>
+        <h2>{props.name.first} {props.name.last}</h2>
+        <p><strong>{props.major}</strong></p>
+        <p>{props.name.first} is taking {props.numCredits} credits and is {props.fromWisconsin ? "" : "NOT"} from Wisconsin </p>
+        <p>They have {props.interests.length} interests including...</p>
+        <ul>
+            {props.interests.map(interest => (
+                <li key={interest}>{interest}</li> 
+            ))}
+        </ul>
+    </div>
+}
+
+export default Student;
